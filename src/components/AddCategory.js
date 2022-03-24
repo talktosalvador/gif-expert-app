a371642b9f95f7dfeaf3a9ffba09c5da2a06483b
Salvador Vilca <salvasvip@gmail.com>
2022-03-23 21:16:25 -0400

    agregando nuevas categorias

---
+++ src/components/AddCategory.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import PropTypes from 'prop-types'
+
+export const AddCategory = ({ setCategorias }) => {
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim().length > 2) {
+      setCategorias((categorias) => [...categorias, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type={"text"} value={inputValue} onChange={handleInputChange} />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+    setCategorias: PropTypes.func.isRequired
+}
--- src/components/GifExpertApp.js
+++ src/components/GifExpertApp.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { AddCategory } from "./AddCategory";
 
 export const GifExpertApp = () => {
+  const [categorias, setCategorias] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball Z",
+  ]);
+
+  // const handleAdd = (nuevaCategoria) => {
+  //   setCategorias([...categorias, nuevaCategoria]);
+  // };
+
   return (
     <>
       <h2>GifExpertApp</h2>
+      <AddCategory setCategorias={setCategorias} />
       <hr />
+      <ol>
+        {categorias.map((categoria) => {
+          return <li key={categoria}>{categoria}</li>;
+        })}
+      </ol>
     </>
   );
 };
